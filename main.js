ee267eac74d3d9ff6f9213dec686742e030c1749
Maria-Latysheva <mlatysheva@gmail.com>
2021-10-16 21:51:27 +0200

    feat: add javascript functionality

--- main.js
+++ main.js
@@ -1,13 +1,15 @@
 const bullets = document.querySelectorAll('.slider-bullet');
-const welcomeContainer = document.getElementById('mems');
+const memText = document.getElementById('mem-text');
+const currentIllustration = document.getElementById('current-illustration');
 const imageArray = ['./assets/mem-slider-0.png', './assets/mem-slider-1.png', './assets/mem-slider-2.png',
 './assets/mem-slider-3.png'];
+const proverbsArray = ['Молод - гложи кости, а стар - ешь кашу.', 'Локоть близко, да не укусишь.', 'Сшито на одного, так на двоих не наденешь.', 'Федул всех надул, а Фетис на суку повис.']
 
 // change images by clicking on the bullets
 bullets.forEach((bullet, index) => {
   bullet.addEventListener('click', () => {
-    mems.style.backgroundImage = `url(${imageArray[index]})`;
-    welcomeContainer.style.transition = "0.4s ease";
+    currentIllustration.src = `${imageArray[index]}`;
+    memText.innerHTML = `${proverbsArray[index]}`;
     bullets.forEach((element) => {
       element.classList.remove("active")
     })
